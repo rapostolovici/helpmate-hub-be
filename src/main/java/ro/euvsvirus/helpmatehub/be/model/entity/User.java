e40471e294727ba0740c54a5d8ba33c87de6c3cd
Radmila Apostolovici <radmila.apostolovici@gmail.com>
2020-04-26 11:40:10 +0300

    Add user category

---
+++ src/main/java/ro/euvsvirus/helpmatehub/be/model/entity/User.java
@@ -0,0 +1,40 @@
+package ro.euvsvirus.helpmatehub.be.model.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import ro.euvsvirus.helpmatehub.be.model.UserCategory;
+
+
+@Schema(description = "The class which models a user")
+@Table(name = "t_user")
+@Entity
+public class User extends PanacheEntity {
+    
+    @Column(name = "first_name")
+    public String firstName;
+    @Column(name="last_name")
+    public String lastName;
+
+    @Enumerated
+    @Column(name = "user_category")
+    private UserCategory userCategory;
+
+
+    //TODO RAP: move to a base entity in the future
+    //TODO RAP: create the sql migration script to add the columns to the user table
+
+    // @CreationTimestamp
+    // public LocalDate creationDate;
+    // @UpdateTimestamp
+    // public LocalDate updatedDate;
+}
